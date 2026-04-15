e9bd17b9a52c709f42d3be3a8d9a3d49194af8a0
Yared <yaryene@gmail.com>
2026-04-15 17:00:06 +0300

    add authservice impl

--- src/main/java/com/herani/ussd_demo/controller/UssdController.java
+++ src/main/java/com/herani/ussd_demo/controller/UssdController.java
@@ -1,5 +1,6 @@
 package com.herani.ussd_demo.controller;
 
+import com.herani.ussd_demo.service.AuthService;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -15,11 +16,14 @@ import java.util.Map;
 @RestController
 @RequestMapping("/ussd")
 public class UssdController {
+    private final AuthService authService;
+
+    public UssdController(AuthService authService) {
+        this.authService = authService;
+    }
+
     @PostMapping
-    public String handleUssd(
-        @RequestParam String sessionId,
-        @RequestParam String phoneNumber,
-        @RequestParam String text) {
+    public String handleUssd(@RequestParam String sessionId, @RequestParam String phoneNumber, @RequestParam String text) {
 
         String[] inputs = text.split("\\*");
 
@@ -32,7 +36,7 @@ public class UssdController {
         if(inputs.length == 1){
             String pin = inputs[0];
 
-            boolean isValid = authenticateUser(pin);
+            boolean isValid = authService.authenticateUser(pin);
             if(isValid){
                 return "CON 1. My Account\n2. Own Account Transfer";
             }
@@ -58,22 +62,22 @@ public class UssdController {
 //    0932948548
     // call your real api
     // RestTemplate (simple for now)
-    private boolean authenticateUser(String pin) {
-        RestTemplate restTemplate = new RestTemplate();
-        String url = "https://qaapicbesuperapp.eaglelionsystems.com/api/v1/cbesuperapp/member_auth/login";
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("device_uuid", "924224a1ae5b3efc");
-        headers.set("platform", "android");
-        headers.set("installation_date", "2026-03-04T12:38:01.627Z");
-        headers.set("x-source", "app");
-        headers.set("app_version", "1.0");
-
-        Map<String, String> body = Map.of("pin", pin);
-
-        HttpEntity<Map<String, String>> request = new HttpEntity<>(body, headers);
-        ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
-        return response.getStatusCode().is2xxSuccessful();
-        // Add any required headers here
-        // headers.set("Authorization", "Bearer " + token);
-    }
+//    private boolean authenticateUser(String pin) {
+//        RestTemplate restTemplate = new RestTemplate();
+//        String url = "https://qaapicbesuperapp.eaglelionsystems.com/api/v1/cbesuperapp/member_auth/login";
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.set("device_uuid", "924224a1ae5b3efc");
+//        headers.set("platform", "android");
+//        headers.set("installation_date", "2026-03-04T12:38:01.627Z");
+//        headers.set("x-source", "app");
+//        headers.set("app_version", "1.0");
+//
+//        Map<String, String> body = Map.of("pin", pin);
+//
+//        HttpEntity<Map<String, String>> request = new HttpEntity<>(body, headers);
+//        ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
+//        return response.getStatusCode().is2xxSuccessful();
+//        // Add any required headers here
+//        // headers.set("Authorization", "Bearer " + token);
+//    }
 }
